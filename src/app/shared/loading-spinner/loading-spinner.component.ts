411b253e0799b079f9b62f14d6c9b052442b9a71
Naximus <nachobakalov@gmail.com>
2022-04-14 19:21:29 +0300

    add auth
    Adding log in logout,
    Gard links

--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-loading-spinner',
   template:
-    '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>',
+    '<div class="loadingio-spinner-rolling-b7drk619q4f"><div class="ldio-14r8p5ivve3"><div></div></div></div>',
   styleUrls: ['./loading-spinner.component.css']
 })
 export class LoadingSpinnerComponent {}
